1cb2ab69a3a865cfd0098baca5d3c8bcab4cefa3
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-09 20:01:00 -0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { Event } from "./pages/Event";
 import "./styles/global.css";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
-
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  console.log(data);
-  return (
-    <div>
-      {data?.lessons.map((lesson) => {
-        return (
-          <h1 key={lesson.id} className="text-2xl text-violet-900">
-            {lesson.title}
-          </h1>
-        );
-      })}
-    </div>
-  );
+  return <Event />;
 }
 
 export default App;
